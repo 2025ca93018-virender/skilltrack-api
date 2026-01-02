2051b3c95ba8c1d45bada3294f9b6c52d88fe1a6
Madhav Singh <2025ca93005@wilp.bits-pilani.ac.in>
2026-01-02 22:31:25 +0530

    Create structure

---
+++ skilltrack/src/test/java/com/example/skilltrack/SkilltrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skilltrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilltrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
